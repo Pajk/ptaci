64eb8eb7e72217fe28ba8c94969473b75bfe83ea
Pavel Pokorny <pajkycz@gmail.com>
2011-12-01 07:31:09 +0100

    Initial Commit

---
+++ ptaci/HelloWorldLayer.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  ptaci
+//
+//  Created by Pavel Pokorny on 12/1/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
