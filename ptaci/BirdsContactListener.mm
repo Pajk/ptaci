4b441c2dfe65d2c93b0c95adb7e1f5094c486377
Pavel Pokorny <pajkycz@gmail.com>
2011-12-03 11:42:00 +0100

    universal contact listener

--- ptaci/BirdsContactListener.mm
+++ ptaci/BirdsContactListener.mm
@@ -9,17 +9,23 @@
 #import "BirdsContactListener.h"
 
 #import "cocos2d.h" 
+#import "Bird.h"
 
 void BirdsContactListener::BeginContact(b2Contact* contact)
 {
     b2Body* bodyA = contact->GetFixtureA()->GetBody();
     b2Body* bodyB = contact->GetFixtureB()->GetBody();
+    
     CCSprite* spriteA = (CCSprite*)bodyA->GetUserData();
     CCSprite* spriteB = (CCSprite*)bodyB->GetUserData();
-
-    if (spriteA != NULL && spriteB != NULL) {
-        spriteA.color = ccMAGENTA;
-        spriteB.color = ccMAGENTA;
+    
+    if ([spriteA isKindOfClass:[Bird class]] &&
+        [spriteB isKindOfClass:[Bird class]]) {
+        
+        Bird *birdA = (Bird*)spriteA;
+        Bird *birdB = (Bird*)spriteB;
+        birdA.color = ccMAGENTA;
+        birdB.color = ccMAGENTA;
     }
 }
 
