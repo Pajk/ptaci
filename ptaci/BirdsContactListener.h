536d258cf057e6ecb52b2931342e3d479d964aee
Pavel Pokorny <pajkycz@gmail.com>
2011-12-03 19:34:57 +0100

    counting score

--- ptaci/BirdsContactListener.h
+++ ptaci/BirdsContactListener.h
@@ -7,10 +7,29 @@
 //
 
 #import "Box2D.h"
+#import <vector>
+#import <algorithm>
 
-class BirdsContactListener : public b2ContactListener { 
-    
-private:
-    void BeginContact(b2Contact* contact);
-    void EndContact(b2Contact* contact);
+struct BirdsContact {
+    b2Fixture *fixtureA;
+    b2Fixture *fixtureB;
+    bool operator==(const BirdsContact& other) const {
+        return (fixtureA == other.fixtureA) && (fixtureB == other.fixtureB);
+    }
 };
+
+class BirdsContactListener : public b2ContactListener {
+    
+public:
+    std::vector<BirdsContact>_contacts;
+    
+    BirdsContactListener();
+    ~BirdsContactListener();
+    
+	virtual void BeginContact(b2Contact* contact);
+	virtual void EndContact(b2Contact* contact);
+	virtual void PreSolve(b2Contact* contact, const b2Manifold* oldManifold);    
+	virtual void PostSolve(b2Contact* contact, const b2ContactImpulse* impulse);
+    
+};
+
